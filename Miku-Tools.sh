fb8cc64dd175d5d253eca06bf770088e799730e4
KOF136 <90687473+KOF136@users.noreply.github.com>
2023-12-03 12:01:45 +0800

    Update Miku-Tools.sh

--- Miku-Tools.sh
+++ Miku-Tools.sh
@@ -64,12 +64,12 @@ fi
 echo "系统配置检测完成，开始配置网络优化"
 
 cat >> /etc/sysctl.conf << EOF
-net.core.default_qdisc=fq_pie
+net.core.default_qdisc=fq
 net.ipv4.tcp_congestion_control=bbr
-net.core.rmem_max=8388608
-net.core.wmem_max=8388608
-net.ipv4.tcp_rmem=4096 87380 8388608
-net.ipv4.tcp_wmem=4096 16384 8388608
+net.core.rmem_max=16777216
+net.core.wmem_max=16777216
+net.ipv4.tcp_rmem=4096 87380 16777216
+net.ipv4.tcp_wmem=4096 16384 16777216
 net.ipv4.conf.all.route_localnet=1
 net.ipv4.ip_forward=1
 net.ipv4.conf.all.forwarding=1
